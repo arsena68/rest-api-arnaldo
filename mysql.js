9eab051581c3825937c819f336e1f688d0113c25
Arnaldo Sena <arsena68@gmail.com>
2021-05-12 11:25:33 -0300

    retiro conn limit

--- mysql.js
+++ mysql.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
-var pool = mysql.createPool({
-    "connectionLimit" : 1000,
+var pool = mysql.createConnection({
+ //   "connectionLimit" : 1000,
     "user" : process.env.MYSQL_USER,
     "password": process.env.MYSQL_PASSWORD,
     "database" : process.env.MYSQL_DATABASE,
@@ -16,7 +16,7 @@ exports.execute = (query, params=[]) => {
                 reject(error);
             } else {
                 resolve(result)
-            }
+            } 
         });
     })
 }
